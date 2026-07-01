8df6b0a85bdbda63901c94e97be5bbb33ada3352
Rajaram <gootamgootam325@gmail.com>
2026-07-01 11:42:24 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,5 +1,1 @@
-document.getElementById('fillForm').addEventListener('click', () => {
-    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, {action: "fill_form"});
-    });
-});
+
